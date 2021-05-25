9cb4a1e57eff7deacff6301c9f9166f677b6e5e0
wielok <wielochowskiwojciech@gmail.com>
2021-05-25 19:25:37 +0200

    add new UI

--- app/src/main/java/com/example/age_of_empires_wiki_polsl/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/age_of_empires_wiki_polsl/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh massa. Aenean ut scelerisque tellus. Morbi porttitor tortor ligula, a blandit lectus ornare nec. Donec id cursus lorem. Vestibulum suscipit risus dui, vel elementum dolor hendrerit a. Nunc erat purus, lobortis nec laoreet vel, condimentum nec arcu. Suspendisse imperdiet sapien ut felis rutrum, in placerat enim ultricies. Mauris ac elit neque. Suspendisse odio velit, varius ultrices est vitae, tincidunt placerat tortor. Integer blandit ante porttitor ante volutpat tempus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec nec purus arcu. Praesent at arcu eu velit posuere facilisis. Pellentesque vel semper quam.\n"
     }
     val text: LiveData<String> = _text
 }
